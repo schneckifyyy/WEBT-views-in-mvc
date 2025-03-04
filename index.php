91023abd2e6702c65f480796fa05bf1716a1327f
schneckifyyy <adamogris@outlook.at>
2025-03-04 09:06:35 +0100

    Add User Story 3

--- index.php
+++ index.php
@@ -2,27 +2,30 @@
 require_once 'vendor/autoload.php';
 use Adamogris\WebtViewsInMvc\Model\Entity\Hotel;
 
-$hotels = [new Hotel("Seaside Paradise",
-    "Located in the heart of the city, Hotel Paradise offers breathtaking views and world-class amenities to make your stay unforgettable.",
-    "src/images/hotelParadise.jpg"),
-    new Hotel("Ocean View Resort",
-        "Wake up to the sound of the waves at Ocean View Resort, where luxury meets the tranquility of the sea.",
-        "src/images/OceanViewResort.jpg"),
-    new Hotel("Mountain Escape Lodge",
-        "Surrounded by stunning mountains, this lodge is the perfect getaway for those seeking adventure and relaxation.",
-        "src/images/mountainsEscapeLodge.jpg"),];
-$template = fread(fopen('src/templates/index.html', 'r'), filesize('src/templates/index.html'));
+$hotels = [
+    new Hotel("Seaside Paradise", "Located in the heart of the city, Hotel Paradise offers breathtaking views and world-class amenities.", "src/images/hotelParadise.jpg"),
+    new Hotel("Ocean View Resort", "Wake up to the sound of the waves at Ocean View Resort, where luxury meets the tranquility of the sea.", "src/images/OceanViewResort.jpg"),
+    new Hotel("Mountain Escape Lodge", "Surrounded by stunning mountains, this lodge is the perfect getaway for those seeking adventure and relaxation.", "src/images/mountainsEscapeLodge.jpg")
+];
 
-$tempReplaced = str_replace("###Hotel1Name###", $hotels[0]->getName(), $template);
-$tempReplaced = str_replace("###Hotel2Name###", $hotels[1]->getName(), $tempReplaced);
-$tempReplaced = str_replace("###Hotel3Name###", $hotels[2]->getName(), $tempReplaced);
+// Read the template
+$template = file_get_contents('src/templates/index.html');
 
-$tempReplaced = str_replace("###Hotel1Description###", $hotels[0]->getDescription(), $tempReplaced);
-$tempReplaced = str_replace("###Hotel2Description###", $hotels[1]->getDescription(), $tempReplaced);
-$tempReplaced = str_replace("###Hotel3Description###", $hotels[2]->getDescription(), $tempReplaced);
+// Extract the loop block
+preg_match('/{{HOTEL_LOOP}}(.*?){{HOTEL_LOOP_END}}/s', $template, $matches);
+$hotelTemplate = $matches[1] ?? '';
 
-$tempReplaced = str_replace("###Hotel1Pic###", $hotels[0]->getImgPath(), $tempReplaced);
-$tempReplaced = str_replace("###Hotel2Pic###", $hotels[1]->getImgPath(), $tempReplaced);
-$tempReplaced = str_replace("###Hotel3Pic###", $hotels[2]->getImgPath(), $tempReplaced);
+$hotelHtml = '';
 
-echo $tempReplaced;
+foreach ($hotels as $hotel) {
+    $hotelEntry = str_replace("{{HOTEL_NAME}}", $hotel->getName(), $hotelTemplate);
+    $hotelEntry = str_replace("{{HOTEL_DESCRIPTION}}", $hotel->getDescription(), $hotelEntry);
+    $hotelEntry = str_replace("{{HOTEL_IMAGE}}", $hotel->getImgPath(), $hotelEntry);
+    $hotelHtml .= $hotelEntry;
+}
+
+// Replace the loop block in the template
+$template = preg_replace('/{{HOTEL_LOOP}}(.*?){{HOTEL_LOOP_END}}/s', $hotelHtml, $template);
+
+echo $template;
+?>
